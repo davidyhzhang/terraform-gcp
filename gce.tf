2669ba52277cb0dd4f793bae59d786fc17720123
davidyhzhang <37316273+davidyhzhang@users.noreply.github.com>
2020-10-12 17:43:28 -0400

    Update gce.tf

--- gce.tf
+++ gce.tf
@@ -8,7 +8,8 @@ resource "google_compute_instance" "default" {
  name         = "flask-vm-${random_id.instance_id.hex}"
  machine_type = "f1-micro"
  zone         = "us-west1-a"
-
+ allow_stopping_for_update = true
+ 
  boot_disk {
    initialize_params {
      image = "debian-cloud/debian-9"
