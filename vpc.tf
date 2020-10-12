81e26fc872208434991904191086da1c66ebd0fc
davidyhzhang <37316273+davidyhzhang@users.noreply.github.com>
2020-10-12 17:30:44 -0400

    Update vpc.tf
    Use self_link to refer to vpc network

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,14 @@
+resource "google_compute_network" "vpc_network" {
+  name                    = "terraform-network"
+  auto_create_subnetworks = "true"
+}
+
 resource "google_compute_firewall" "default" {
  name    = "flask-app-firewall"
- network = "default"
+ network = google_compute_network.vpc_network.self_link
 
  allow {
    protocol = "tcp"
-   ports    = ["5000"]
+   ports    = ["5000", "22", "80", "443", "8080"]
  }
 }
